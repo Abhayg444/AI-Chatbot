c1b486458483700dc33dd5ba69e4ff3a536f7d94
Abhay Gupta <89296170+Abhayg444@users.noreply.github.com>
2023-03-24 16:56:57 +0000

    Basic import from Serenity

--- train.py
+++ train.py
@@ -1,11 +1,11 @@
 import numpy as np
 import random
 import json
-
+import nltk
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
-
+nltk.download('punkt')
 from nltk_utils import bag_of_words, tokenize, stem
 from model import NeuralNet
 
